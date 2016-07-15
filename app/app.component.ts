f04ec6e0b1d6c7558fdb8886326d52a4133325b2
Joseph M J <joseph.mj@cordiant.com>
2016-07-15 19:02:58 +0530

    Hi, Angular2 is here..

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hi,</h1><h3>Angular2 is here...</h3>'
+})
+export class AppComponent { }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ readme.md
@@ -0,0 +1,11 @@
+## **Hi,**
+### Angular2 is here
+### Just used CDN for Angular2 libraries.
+
+`git clone https://github.com/jmjangular2/JustHi.git`
+
+
+
+
+#### Explore Angular2
+_JMJ_
